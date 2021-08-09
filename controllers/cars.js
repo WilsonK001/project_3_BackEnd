74617047a4a9f332f530b7102ed0f5e2a186f38e
Wilson Kameni <wilsonk@wilsons-mbp.myfiosgateway.com>
2021-08-09 14:59:08 -0400

    :id on the PUT route

--- controllers/cars.js
+++ controllers/cars.js
@@ -39,7 +39,7 @@ router.delete('/:id', (req, res)=>{
 
 
 //////UPDATE ROUTE/////
-router.put('/', (req, res)=>{
+router.put('/:id', (req, res)=>{
     Car.findByIdAndUpdate(req.params.id, (err, updatedCar)=>{
         res.json(updatedCar)
     })
