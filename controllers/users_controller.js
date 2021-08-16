009aeae333b434a5047fac4fe327f3c9c66ce88d
Wilson Kameni <wilsonk@wilsons-mbp.myfiosgateway.com>
2021-08-16 09:14:20 -0400

    ..

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -6,18 +6,14 @@ const user = require('../models/users.js')
 
 ///Users Routes, get and post
 users.get('/', (req, res)=>{
-    res.send("Users controller")
-    /*
     user.find({}, (err, foundUser)=>{
         res.json(foundUser)
-        
     })
-    */
+    
 })
 
 
 users.post('/', (req, res)=>{
-    console.log("creating a new user")
     user.create(req.body, (err, createdUser)=>{
         res.json(createdUser)
     })
