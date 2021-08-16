43d2b37b2f62e96c31a6cc703e32016719ed9a19
Wilson Kameni <wilsonk@wilsons-mbp.myfiosgateway.com>
2021-08-16 09:34:01 -0400

    ...

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,25 +1,64 @@
 const bcrypt = require('bcrypt')
-const e = require('express')
 const express = require('express')
 const users = express.Router()
-const user = require('../models/users.js')
+const User = require('../models/users.js')
 
 
 ///Users Routes, get and post
 users.get('/', (req, res)=>{
-    user.find({}, (err, foundUser)=>{
-        if (err)
-            next(err)
-        else
-            res.send(foundUser)
+    User.find({}, (err, foundUser)=>{
+        res.json(foundUser)
+        
     })
-    
 })
 
-
+// signup part
+const saltRounds = 10
 users.post('/', (req, res)=>{
-    user.create(req.body, (err, createdUser)=>{
-        res.json(createdUser)
+    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+        User.create({
+            username: username,
+            password: hash
+        }, (err, createdUser)=>{
+            res.json(createdUser)
+        })
+
+    })
+})
+
+// login part
+users.post('/log/', (req, res)=>{
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    }).then((User) => {
+        if (!User) {
+            // username not found
+            res.json('false')
+        }
+        else {
+            bcrypt.compare(req.body.password, User.password, (err, result) => {
+                if (result == true) {
+                    // correct password
+                    res.json(true)
+                }
+                else {
+                    //incorrect
+                    res.json(false)
+                }
+            })
+        }
+
+    })
+    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+        User.create({
+            username: username,
+            password: hash
+        }, (err, createdUser)=>{
+            res.json(createdUser)
+        })
+
     })
 })
 
--- models/users.js
+++ models/users.js
@@ -1,65 +1,10 @@
-const bcrypt = require('bcrypt')
-const express = require('express')
-const users = express.Router()
-const user = require('../models/users.js')
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-
-///Users Routes, get and post
-users.get('/', (req, res)=>{
-    user.find({}, (err, foundUser)=>{
-        res.json(foundUser)
-        
-    })
+const userSchema = Schema({
+    username: {type: String, unique: true, required: true},
+    password: String
 })
 
-// signup part
-const saltRounds = 10
-users.post('/create', (req, res)=>{
-    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-        user.create({
-            username: username,
-            password: hash
-        }, (err, createdUser)=>{
-            res.json(createdUser)
-        })
-
-    })
-})
-
-// login part
-users.post('/', (req, res)=>{
-    usre.findOne({
-        where: {
-            username: req.body.username
-        }
-    }).then((user) => {
-        if (!user) {
-            // username not found
-            res.json('false')
-        }
-        else {
-            bcrypt.compare(req.body.password, user.password, (err, result) => {
-                if (result == true) {
-                    // correct password
-                    res.json(true)
-                }
-                else {
-                    //incorrect
-                    res.json(false)
-                }
-            })
-        }
-
-    })
-    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-        user.create({
-            username: username,
-            password: hash
-        }, (err, createdUser)=>{
-            res.json(createdUser)
-        })
-
-    })
-})
-
-module.exports = users
+const User = mongoose.model('User', userSchema)
+module.exports = User
